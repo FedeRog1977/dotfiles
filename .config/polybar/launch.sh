cdb4d1dc412c4df5a517bfbbf379b6addaefe2e2
Lewis Britton <lewisbritton23@yahoo.co.uk>
2022-10-10 10:23:10 +0100

    ..

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,15 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Manage multiple monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
 # Launch
 while pgrep -u $UID -x polybar >dev/null; do sleep 1; done
 polybar brittonsbar &
